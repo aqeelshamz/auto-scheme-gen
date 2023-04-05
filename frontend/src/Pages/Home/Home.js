7b8bf47324ce1ef4e745be0f8b1000b4aa8f614c
anuragrajanp <anuragrajanp@gmail.com>
2023-04-05 11:44:34 +0530

    header

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,7 +1,13 @@
+import "./Home.css";
+
 function Home() {
-  return (
-    <div>Home</div>
-  )
+	return (
+		<div>
+			<button id="button" onClick={() => (window.location.href = "/Scheme")}>
+				New Scheme
+			</button>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
