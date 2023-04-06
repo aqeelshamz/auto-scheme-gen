bec57a3c77fcf9d6b75d27a667f1e474afaf2e84
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-04-06 05:46:32 +0530

    b: update initial config

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+//* CONFIGURATIONS
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+//* MONGOOSE CONFIGURATION
+mongoose.set("strictQuery", true);
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
