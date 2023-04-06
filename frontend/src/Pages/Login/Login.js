fbc34085be0c547bd475eed48c8b46d43696aa13
anuragrajanp <anuragrajanp@gmail.com>
2023-04-06 17:40:45 +0530

    minor

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./login.css";
+import "./Login.css";
 
 const Login = (props) => {
 	const [email, setEmail] = useState("");
--- frontend/src/Pages/Login/Register.js
+++ frontend/src/Pages/Login/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "/login.css"
+import "/Login.css"
 
 const Register = (props) => {
 	const [email, setEmail] = useState("");
