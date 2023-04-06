5376651ed71671a6a08584ac1b00ca1ff30b14e4
Anurag <anuragrajanp@gmail.com>
2023-04-06 17:12:30 +0530

    login and register

---
+++ frontend/src/Pages/Login/Login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const Login = (props) => {
+	const [email, setEmail] = useState("");
+	const [pass, setPass] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email);
+	};
+
+	return (
+		<div className="auth-form-container">
+			<div className="column" style={{ paddingBottom: "20px"}}>
+				<p style={{ fontSize: "2rem", fontWeight: "800" }}>SchemeGen</p>
+				<p
+					style={{
+						fontSize: "0.8rem",
+						fontWeight: "500",
+					}}
+				>
+					THRISSUR CITY POLICE
+				</p>
+			</div>
+			<h2>Log In</h2>
+			<form className="login-form" onSubmit={handleSubmit}>
+				{/* <label htmlFor="email">Email</label> */}
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					placeholder="Email address"
+					id="email"
+					name="email"
+				/>
+				{/* <label htmlFor="password">Password</label> */}
+				<input
+					value={pass}
+					onChange={(e) => setPass(e.target.value)}
+					type="password"
+					placeholder="Password"
+					id="password"
+					name="password"
+				/>
+				<button type="submit">Log In</button>
+				<button
+					className="link-btn"
+					onClick={() => props.onFormSwitch("register")}
+				>
+					Don't have an account? Register
+				</button>
+			</form>
+		</div>
+	);
+};
+export default Login;
---
+++ frontend/src/Pages/Login/Register.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+const Register = (props) => {
+	const [email, setEmail] = useState("");
+	const [pass, setPass] = useState("");
+	const [name, setName] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email);
+	};
+
+	return (
+		<div className="auth-form-container">
+			<div className="column" style={{ paddingBottom: "20px" }}>
+				<p style={{ fontSize: "2rem", fontWeight: "800" }}>SchemeGen</p>
+				<p
+					style={{
+						fontSize: "0.8rem",
+						fontWeight: "500",
+					}}
+				>
+					THRISSUR CITY POLICE
+				</p>
+			</div>
+			<h2>Register</h2>
+			<form classname="register-form" onSubmit={handleSubmit}>
+				{/* <label htmlFor="name">Name</label> */}
+				<input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					type="text"
+					placeholder="Name"
+					id="name"
+					name="name"
+				/>
+				{/* <label htmlFor="email">Email</label> */}
+				<input
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type="email"
+					placeholder="Email address"
+					id="email"
+					name="email"
+				/>
+				{/* <label htmlFor="password">Password</label> */}
+				<input
+					value={pass}
+					onChange={(e) => setPass(e.target.value)}
+					type="password"
+					placeholder="New password"
+					id="password"
+					name="password"
+				/>
+				<button type="submit">Register</button>
+				<button
+					className="link-btn"
+					onClick={() => props.onFormSwitch("login")}
+				>
+					Already have an account? Log In
+				</button>
+			</form>
+		</div>
+	);
+};
+export default Register;
