7046b0cc6fe1ecdf51be18f653ab3e782788e067
anuragrajanp <anuragrajanp@gmail.com>
2023-04-09 16:03:43 +0530

    axios 01

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Scheme from "./Pages/Scheme/Scheme";
 import Login from "./Pages/Login/Login";
 import Home from "./Pages/Home/Home";
-import Register from "./Pages/Login/Register";
+import Signup from "./Pages/Login/Signup";
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
 				<Routes>
 					<Route exact path="/" element={<Scheme />} />
 					<Route exact path="/login" element={<Login />} />
-					<Route exact path="/register" element={<Register />} />
+					<Route exact path="/signup" element={<Signup />} />
 					<Route exact path="/home" element={<Home />} />
 				</Routes>
 			</Router>
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,4 @@
 const googleApiKey = "AIzaSyBFrn8O362KFIOQWHGmcuiwjpfZsUXig-k";
+{/*const serverURL = "http://localhost:6001";*/}
 
 export default googleApiKey;
