105e9076eb9f9ee8f0c2b87aa72d7bbc071b224a
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-05-20 14:19:25 +0530

    b: Added last opened event post, get routes

--- backend/src/util/nodeMailer.js
+++ backend/src/util/nodeMailer.js
@@ -1,27 +1,25 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 //implement nodemailer
-
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'your-email@gmail.com',
-      pass: 'your-password' //
-    }
-  });
-  
+  service: "gmail",
+  auth: {
+    user: "your-email@gmail.com",
+    pass: "your-password", 
+  },
+});
+
 const mailOptions = {
-    from: 'your-email@gmail.com',
-    to: 'recipient-email@example.com',
-    subject: 'Test email',
-    text: 'This is a test email from nodemailer.'
-  };
-  
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.error(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-  
+  from: "your-email@gmail.com",
+  to: "recipient-email@example.com",
+  subject: "Test email",
+  text: "This is a test email from nodemailer.",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
