2235a1467892197355ede32bf88aeb5baa25ec10
anuragrajanp <anuragrajanp@gmail.com>
2023-08-28 16:38:46 +0530

    EventTable init

--- frontend/src/Pages/Finish/Finish.jsx
+++ frontend/src/Pages/Finish/Finish.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../../components/Navbar/Navbar";
+import EventTable from "../../components/EventTable/EventTable";
 
 const Finish = () => {
 	return !localStorage.getItem("token") &&
@@ -6,22 +7,24 @@ const Finish = () => {
 		window.location.pathname !== "/signup" ? (
 		<></>
 	) : (
-		<div className="overflow-hidden">
+		<div className="overflow-hidden mt-10">
 			<Navbar />
-			<div className="min-h-screen mt-24  w-full">
-				<div className="m-4">
-					<div className="flex justify-between">
-						<p className="font-semibold text-xl">Finish Event</p>
+			<div className="min-h-screen mt-24  w-full justify-center">
+				<div className="m-4 justify center">
+					<div className="flex justify-center">
+						<h1 className="font-bold text-3xl">Finish Event</h1>
 					</div>
-					<br />
-					<h1>EVENT DETAILS HERE</h1>
-					<br />
-					<button className="block w-[424px] h-[35px] mt-10 px-4 py-2 bg-blue-900 text-white  rounded-md text-xs text-center">
-						Download in PDF
-					</button>
-					<button className="block w-[424px] h-[35px] mt-10 px-4 py-2 bg-blue-900 text-white  rounded-md text-xs text-center">
-						Download in XLSX
-					</button>
+					<div className="justify-center flex my-10">
+						<EventTable />
+					</div>
+					<div className="justify-center flex">
+						<button className="block w-[424px] h-[35px] mt-5 px-4 py-2 bg-blue-900 text-white rounded-md text-xs text-center">
+							Download in PDF
+						</button>
+						<button className="block w-[424px] h-[35px] mt-5 px-4 py-2 bg-blue-900 text-white rounded-md text-xs text-center">
+							Download in XLSX
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
